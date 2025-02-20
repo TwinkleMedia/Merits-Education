afbecf2d8c96101d2469719b3e7dfe8350fec82a
TwinkleMedia <hs0052264@gmail.com>
2025-02-20 10:58:29 +0530

    Update

---
+++ link.php
@@ -0,0 +1,12 @@
+<!-- Bootstrap file link -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+
+
+
+
+<!-- Css add -->
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="mediaquery.css">
+<link rel="stylesheet" href="navbar.css">
