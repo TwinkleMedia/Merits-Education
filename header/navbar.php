1b4470106d9c135d45f2b07efaef7979f3d2827e
ada2003 <hs0052264@gmail.com>
2025-03-01 17:07:47 +0530

    admin login code  config

--- header/navbar.php
+++ header/navbar.php
@@ -1,7 +1,7 @@
 <header>
         <nav class="navbar navbar-expand-lg">
             <div class="container">
-                <img src="../assest/company logo/merits logo.png" alt="">
+                <img src="../assest/company logo/merits logo.png" style="width: 80px;" alt="">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
