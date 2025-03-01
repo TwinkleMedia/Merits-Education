4bc854cd2239bad954886f5372f5929bc98521bd
ada2003 <hs0052264@gmail.com>
2025-03-01 17:19:08 +0530

    admin chang

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<?php include './login/login.php';?>
+<?php include './admin/login/login.php';?>
 
 </body>
 </html>
