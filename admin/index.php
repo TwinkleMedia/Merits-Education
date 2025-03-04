0422d17812f9b0a80e79f60167b14cff579ca8a3
ada2003 <hs0052264@gmail.com>
2025-03-04 10:58:44 +0530

    accha Commit

--- admin/index.php
+++ admin/index.php
@@ -67,6 +67,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <title>Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./login/login.css">
 </head>
 <body>
 <div class="container mt-5">
