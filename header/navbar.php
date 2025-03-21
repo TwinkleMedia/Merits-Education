35c990c84b459e68485f4d20c5ebdab05305e7d9
TwinkleMedia <hs0052264@gmail.com>
2025-03-21 14:24:14 +0530

    update-nav

--- header/navbar.php
+++ header/navbar.php
@@ -2,9 +2,9 @@
         <nav class="navbar navbar-expand-lg">
             <div class="container">
                 <img src="./assest/company logo/merits logo.png" style="width: 80px;" alt="">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <i class="bi bi-list"></i>
+</button>
                 <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
@@ -12,6 +12,7 @@
                         <li class="nav-item"><a class="nav-link" href="#coaching">Coaching</a></li>
                         <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
                     </ul>
+                    
                 </div>
                 <div class="ms-auto">
                     <a href="tel:+919987276315" class="btn btn-primary">Call to Us</a>
